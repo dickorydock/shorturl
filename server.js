8c1d95a1ec9b6ba8a7b470a00281878f0ee179b3
Dickory Dock <nickcommins@gmail.com>
2016-08-03 11:29:21 -0400

    basic JSON output new

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/new/:name', function(req,res){
     // http.get(req.params.name, function(data){
         // console.log(data);
     // });
-    http.get("www.google.com", function(thisreq){
+    http.get("http://www.google.com", function(thisreq){
         console.log(thisreq.statusCode);
     });
     // http.get(req.params.name, function(data){
