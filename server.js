620c0751af58df65d85c0e132ea271f3ec75de00
Dickory Dock <nickcommins@gmail.com>
2016-08-04 00:08:04 -0400

    error handling throw err

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.get('/new/http://:name', function(req,res){
     });
     http.get("http://"+req.params.name, function(err, data){
         if ((err) || (thisreq.statusCode != 200)){
+            console.log(err);
+            throw err;
             mystatus = "Could not load";
         }
         else {
