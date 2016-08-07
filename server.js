38e345d7e15dd85bbe44ab098186e2778b2d76fd
Dickory Dock <nickcommins@gmail.com>
2016-08-07 12:24:10 -0400

    updated url shortener app

--- server.js
+++ server.js
@@ -10,100 +10,102 @@ var mystatus="";
 var mongo = require('mongodb').MongoClient;
 
 app.get('/new/:name*', function(req,res){
-    console.log("worked")
     //detect if name is a URL
-    //return output
+    //if it is not a proper http, return 'could not load' status
     if (req.params.name.substr(0,4).toLowerCase() != "http")
-    {
-    mystatus = "Could not load";
-    res.json({url_part1: req.params.name,
-        url_part2: req.params[0],
-        status: mystatus
-        });
+        {
+        mystatus = "Could not load";
+        res.json({url_part1: req.params.name,
+                 url_part2: req.params[0],
+                 status: mystatus
+            });
         
-    }
+        }
     http.get(req.params.name+req.params[0], function(thisres){
-        if (thisres.statusCode != 200){
-            mystatus = "Could not load";
-        }
-        else {
-            mystatus = "Loaded!";
-        }
-    
-    res.json({url_part1: req.params.name,
-        url_part2: req.params[0],
-        status: mystatus
-        });
-    }).on('error', function(e){
-        console.error(e.code);
-    mystatus = "Could not load";
-    res.json({url_part1: req.params.name,
-        url_part2: req.params[0],
-        status: mystatus
-        });
-    
+        var wwwpath = req.params[0].substr(2, req.params[0].length-2);
 
+        //do not look for the site unless it returns a 200 status code
+        if (thisres.statusCode != 200){
+            loadstatus = "Could not load";
+        }
+
+        //otherwise, find the site in the MongoDB list!
+        else {
+            //indicate that the site is a working URL
+            loadstatus = "Loaded!";
+
+            //prepare the database
+            mongo.connect("mongodb://dickorydock:$iderHouseRul3z@ds145365.mlab.com:45365/urlrosetta", function(err, db) {
+                var shorturl = db.collection("shorturl");
+                //find maximum site number
+                shorturl.find().sort({site_number:-1}).limit(1).toArray(function(err,documents){
+                    max_site_number = documents[0]["site_number"];
+                })
+                //look for the site in the existing database
+                shorturl.find(
+                   {original_url: wwwpath}
+                 , {_id: 0, original_url: 1,short_url: 1, site_number:1})
+                .toArray(function(err,documents){    
+                    //if we found it, no need to add it again -- just return the existing site number
+                    if (documents.length>0){
+                        var sitenumber = documents[0]["site_number"];
+                    }
+                    //if we didn't find the working URL, add it
+                    else {
+                        var sitenumber = max_site_number + 3;
+                        var newsitejson={original_url: wwwpath, short_url: "makeme", site_number: sitenumber}
+                        shorturl.insert(newsitejson)
+                    }
+
+                    //return the info about the short link
+                    res.json({original_url:  req.params.name+"://"+wwwpath,
+                        short_url: "http://dickorydock-shorturl.herokuapp.com/"+sitenumber
+                        });
+                  db.close()
+                })
+            })
+        }
+    
+    })
+    //if there is an error in finding the site in the URL, return 'could not load' status
+    .on('error', function(e){
+        console.error(e.code);
+        loadstatus = "Could not load";
+             res.json({error: "Not a valid URL - try again."})
+           
     })
 })
 
-app.listen(process.env.PORT, function(){
+
+app.get('/:shortnum*', function(req,res){
+   var sitenumber = parseInt(req.params.shortnum);
+   var siteextra = req.params[0];
+   if (siteextra.length > 0){
+        res.json({error: "Not a valid reference - try again."})
+   }
+   else {
+    mongo.connect("mongodb://dickorydock:$iderHouseRul3z@ds145365.mlab.com:45365/urlrosetta", function(err, db) {
+        var shorturl = db.collection("shorturl");
+        //look for the site in the existing database, and either redirect or give an error
+        shorturl.find(
+           {site_number: sitenumber}
+         , {_id: 0, original_url: 1,short_url: 1, site_number:1})
+        .toArray(function(err,documents){ 
+            if (documents.length>0){
+            res.redirect("http://"+documents[0]["original_url"])
+            }
+            else if (documents.length == 0){
+                res.json({error: "Not a valid reference - try again."})
+            }
+          db.close()
+         })
+
+    })
+
+    }
+
+})
+app.listen(8080/*process.env.PORT*/, function(){
     console.log("App listening")
 });
-
-
-mongo.connect("mongodb://dickorydock:$iderHouseRul3z@ds01316.mlab.com:1316/urlrosetta", function(err, db) {
-  if (err) throw err;
-  var thiscollection = db.collection("shorturl");
-  var jsonobject = {original_url: "mememe", new_url: "youyouyou"};
-  thiscollection.insert(jsonobject, function(err, data){
-   console.log(JSON.stringify(jsonobject));
-    });
-  db.close();
-  });
-
-
-    // mystatus = "no http.get";
-    // var jsonoutput = JSON.stringify({
-    //     original_url: req.params.name,
-    //     short_url: "http://dickorydock-shorturl.herokuapp.com/mememe",
-    //     status: mystatus
-    // })
-//process.env.PORT
-    // console.log("App listening on port "+process.env.PORT)
-    // http.get(req.params.name, function(data){
-        // console.log(data);
-    // });
-    // http.get("http://"+req.params.name, function(thisreq){
-    //     console.log(thisreq.statusCode);
-    // });
-    // http.get(req.params.name+req.params[0], function(err, data){
-
-/*
-app.get('/', function(req, res){
-    var myipaddress = req.headers["x-forwarded-for"];
-    var mylanguage = req.headers["accept-language"].split(",")[0];
-    var mysoftware_pre = req.headers["user-agent"];
-    var mysoftware = mysoftware_pre.slice(mysoftware_pre.indexOf('(') +1,mysoftware_pre.indexOf(')'));
-    var jsonoutput = JSON.stringify({
-                 ipaddress: myipaddress,
-                 language: mylanguage,
-                 software: mysoftware });
-    res.send(jsonoutput);
-})
-
-
-
-app.listen(8080, function(){
-    console.log("Listening on port 8080")
-})
-*/
-
-
-// app.get('/', function(req, res){
-//     jsonoutput = JSON.stringify({unix: null, natural: null});
-//     res.send(jsonoutput);
-// })
-// app.get('/*', function(req,res){
-//     console.log("this");
-// })
 
